4bcebbfa50611c8f20c1b15eee8bf5a9260d1cb9
HrsK22 <148621118+HrsK22@users.noreply.github.com>
2023-10-25 16:29:04 +0700

    Update Limas Segiempat.py

--- Pertemuan 1/Limas Segiempat.py
+++ Pertemuan 1/Limas Segiempat.py
@@ -3,18 +3,19 @@ print("-----------------------------------------------------")
 
 """
 Keterangan:
-LS = Luas Sisi
+LS_1-4 = Luas Sisi
+LS_5 = Luas Persegi
 La = Luas Alas
 t = Tinggi
 """
 
 # Atur Nilai
 print("Masukkan Nilai")
-LS_1 = float(input("Luas Segitiga 1: "))
-LS_2 = float(input("Luas Segitiga 2: "))
-LS_3 = float(input("Luas Segitiga 3: "))
-LS_4 = float(input("Luas Segitiga 4: "))
-LS_5 = float(input("Luas Segitiga 5: "))
+LS_1 = float(input("Luas Sisi 1: "))
+LS_2 = float(input("Luas Sisi 2: "))
+LS_3 = float(input("Luas Sisi 3: "))
+LS_4 = float(input("Luas Sisi 4: "))
+LS_5 = float(input("Luas Persegi: "))
 La = float(input("Luas Alas: "))
 t = float(input("Tinggi: "))
 print("-----------------------------------------------------")
